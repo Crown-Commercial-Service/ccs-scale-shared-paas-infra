ac9148ed624de96353aa1953f67f760a262a6b69
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-01 16:45:26 +0100

    SCC-605 - added terraform and travis build

---
+++ bootstrap/create-tf-state-bucket.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export SERVICE_NAME=terraform-state
+export KEY_NAME=terraform-state-key
+
+#######################
+# Create Terraform State S3 Bucket
+#######################
+cf create-service aws-s3-bucket default $SERVICE_NAME -c '{"public_bucket":false}'
+
+cf create-service-key $SERVICE_NAME $KEY_NAME -c '{"allow_external_access": true}'
---
+++ iac/environments/sbx2/backend.tf
@@ -0,0 +1,17 @@
+#########################################################
+# Config: remote backend
+#
+# Need to supply the sensitive values during `terraform init`
+# 
+# terraform init \
+#    -backend-config="access_key=ACCESS_KEY_ID" \
+#    -backend-config="secret_key=SECRET_ACCESS_KEY" \
+#    -backend-config="bucket=S3_STATE_BUCKET_NAME"
+#########################################################
+terraform {
+  backend "s3" {
+    key    = "shared/infra"
+    region = "eu-west-2"
+  }
+}
+
