086977fe77de9881e879c3f899882557de53e813
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-06 17:12:45 +0100

    SCC-605 - renamed 'terraform' to 'iac'

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #
 
 echo "Executing Terraform scripts to build shared space"
-cd ./terraform/environments/$CF_ENVIRONMENT/
+cd ./iac/environments/$CF_ENVIRONMENT/
 terraform init $tf_init_cli_options -backend-config="access_key=$ACCESS_KEY_ID" -backend-config="secret_key=$SECRET_ACCESS_KEY" -backend-config="bucket=$S3_STATE_BUCKET_NAME"
 terraform validate $tf_validation_cli_options
 terraform apply -auto-approve -input=false -var="cf_username=$CF_USERNAME" -var="cf_password=$CF_PASSWORD"
