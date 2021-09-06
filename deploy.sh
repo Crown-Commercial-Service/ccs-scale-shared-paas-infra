ff2502e8e8e7ee7f4f28e7b9a9ad7561bf680edc
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-06 16:34:40 +0100

    SCC-605 - travis env vars no longer seemed to work

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cd ./tf/environments/$CF_ENVIRONMENT/
 terraform init $tf_init_cli_options -backend-config="access_key=$ACCESS_KEY_ID" -backend-config="secret_key=$SECRET_ACCESS_KEY" -backend-config="bucket=$S3_STATE_BUCKET_NAME"
 terraform validate $tf_validation_cli_options
 #terraform plan $tf_plan_cli_options -var="cf_username=$CF_USERNAME" -var="cf_password=$CF_PASSWORD"
-terraform apply $tf_apply_cli_options -var="cf_username=$CF_USERNAME" -var="cf_password=$CF_PASSWORD"
+terraform apply -auto-approve -input=false -var="cf_username=$CF_USERNAME" -var="cf_password=$CF_PASSWORD"
