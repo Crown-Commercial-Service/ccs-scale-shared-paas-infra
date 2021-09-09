b26ce8af7ed698e8fd02684ae679d071b12cb76e
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-09 23:09:49 +0100

    SCC-715 - tidy up

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@
 #
 
 echo "Executing Terraform scripts to build shared space"
-echo "TF: $TF_VERSION"
-echo "Bucket: $S3_STATE_BUCKET_NAME"
-echo "User: $CF_USERNAME"
-echo "Env: $CF_ENVIRONMENT"
 cd ./iac/environments/$CF_ENVIRONMENT/
 terraform init -backend-config="access_key=$ACCESS_KEY_ID" -backend-config="secret_key=$SECRET_ACCESS_KEY" -backend-config="bucket=$S3_STATE_BUCKET_NAME"
 terraform validate
