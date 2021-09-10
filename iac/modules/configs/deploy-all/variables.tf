accb563d8c69a8887c5c59d9360329052bbda4ca
tommyb82 <thomasbunting@gmail.com>
2021-09-10 17:48:52 +0100

    IP ranges for external access

--- iac/modules/configs/deploy-all/variables.tf
+++ iac/modules/configs/deploy-all/variables.tf
@@ -26,6 +26,12 @@ variable "syslog_drain_url" {}
 
 variable "allowed_ips" {
   default = <<EOT
-allow 0.0.0.0/0;
+allow 195.59.5.128/29;   # TechOps private range
+allow 51.149.9.112/29;   # CCS External VPN
+allow 51.149.9.240/29;   # CCS External VPN
+allow 146.199.240.40/32; # TB
+allow 84.68.181.230/32;  # AM
+allow 203.99.209.0/24;   # Cog External VPN
+allow 18.133.142.117/32; # Cog 'SCALE Test Machine 3' in NFT
 EOT
 }
