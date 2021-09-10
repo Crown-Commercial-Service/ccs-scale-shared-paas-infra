954b6af2d2070d6af28eba5c3626d9526c466a0f
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-10 16:47:12 +0100

    SCC-605 - added debug

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Deploys the CF shared infra
 #
 
-echo "Executing Terraform scripts to build shared space"
+echo "Executing Terraform scripts in [$CF_ENVIRONMENT]"
 cd ./iac/environments/$CF_ENVIRONMENT/
 terraform init -backend-config="access_key=$ACCESS_KEY_ID" -backend-config="secret_key=$SECRET_ACCESS_KEY" -backend-config="bucket=$S3_STATE_BUCKET_NAME"
 terraform validate
