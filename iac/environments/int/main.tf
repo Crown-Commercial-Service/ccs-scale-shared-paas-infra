56e607b3c9544abf192d5535733d0895c04037a9
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2021-09-13 17:04:19 +0100

    Env-specific IP ranges (ENT-API-G) (#9)

--- iac/environments/int/main.tf
+++ iac/environments/int/main.tf
@@ -3,6 +3,12 @@
 #
 # Deploy shared resources
 #########################################################
+locals {
+  env_allowed_ips = <<EOT
+allow 3.9.61.85/32; # Ent-APIG external (TST/INT)
+EOT  
+}
+
 module "deploy-all" {
   source           = "../../modules/configs/deploy-all"
   space            = "int"
@@ -10,4 +16,5 @@ module "deploy-all" {
   cf_username      = var.cf_username
   cf_password      = var.cf_password
   syslog_drain_url = "https://d74f58fc-479f-413d-bb66-1cec772b5f5a-ls.logit.io:17256"
+  env_allowed_ips  = local.env_allowed_ips
 }
--- iac/environments/nft/main.tf
+++ iac/environments/nft/main.tf
@@ -3,6 +3,12 @@
 #
 # Deploy shared resources
 #########################################################
+locals {
+  env_allowed_ips = <<EOT
+allow 3.9.61.85/32; # Ent-APIG external (TST/INT)
+EOT  
+}
+
 module "deploy-all" {
   source           = "../../modules/configs/deploy-all"
   space            = "nft"
@@ -11,4 +17,5 @@ module "deploy-all" {
   cf_password      = var.cf_password
   # This is logit.io DEV stack - update to NFT after it is created (zendesk 28070)
   syslog_drain_url = "https://44f18302-59ca-4034-a82e-63f742e60a3e-ls.logit.io:12732"
+  env_allowed_ips  = local.env_allowed_ips  
 }
