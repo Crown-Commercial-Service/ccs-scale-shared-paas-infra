d20f49b76d24b7316ec0ae45de31a912dc8b8b42
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-10-07 17:02:00 +0100

    Bugfix/logit ssm param (#44)
    Moved sysdrain log url to SSM parameter

--- iac/environments/sbx1/main.tf
+++ iac/environments/sbx1/main.tf
@@ -9,7 +9,6 @@ module "deploy-all" {
   environment                = "sbx1"
   cf_username                = var.cf_username
   cf_password                = var.cf_password
-  syslog_drain_url           = "https://44f18302-59ca-4034-a82e-63f742e60a3e-ls.logit.io:12732"
   ip_router_instances        = 1
   agreements_db_service_plan = "tiny-unencrypted-12-high-iops"
 }
