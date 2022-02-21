ade1f268be7adb7e5a956009e81a60a8d736e179
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2022-02-21 12:00:39 +0000

    TF AWs provider fix (#50)

--- iac/environments/uat/main.tf
+++ iac/environments/uat/main.tf
@@ -4,9 +4,9 @@
 # Deploy shared resources
 #########################################################
 module "deploy-all" {
-  source           = "../../modules/configs/deploy-all"
-  space            = "uat"
-  environment      = "uat"
-  cf_username      = var.cf_username
-  cf_password      = var.cf_password
+  source      = "../../modules/configs/deploy-all"
+  space       = "uat"
+  environment = "uat"
+  cf_username = var.cf_username
+  cf_password = var.cf_password
 }
